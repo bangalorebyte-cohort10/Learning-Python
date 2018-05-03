3d5eb0d6b67814e86ade10312175def861ddd0f4
Lakshmi Boggaram <lakshmi.intechcode@gmail.com>
2018-05-03 13:11:42 +0530

    Module

---
+++ module/module1.py
@@ -0,0 +1,29 @@
+
+# Module defined for cohort 9
+
+import math
+
+def addition(a,b):
+	return a+b
+
+def subtraction(a,b):
+	return a-b
+
+def multiplication(a,b):
+	return a*b
+
+def division(a,b):
+	return a/b
+
+def modulo(a,b):
+	return a%b
+
+def squareroot(a):
+	return math.sqrt(a)	
+
+def power(a,b):
+	return a**b
+
+def power2(a,b):
+	return a**b*2
+
---
+++ module/test.py
@@ -0,0 +1,10 @@
+import module1
+
+print(module1.addition(10,10))
+print(module1.subtraction(30,10))
+print(module1.multiplication(40,90))
+print(module1.division(200,2))
+print(module1.modulo(100,10))
+print(module1.power(10,2))
+print(module1.power2(10,4))
+
